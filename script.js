bc1f879c3c1af671376a5e57b62e06d74050a06f
JoviJustino <joaovictortere@hotmail.com>
2023-10-25 16:03:57 -0300

    final funcionalidade

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 'use strict';
 
-const modal = document.querySelector('.modal')
-const overlay = document.querySelector('.overlay')
-const btnCloseModal = document.querySelector('.close-modal')
-const btnsOpenModal = document.querySelector('.open-modal')
+const modal = document.querySelector('.modal');
+const overLay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function(){
+  modal.classList.remove('hidden');
+  overLay.classList.remove('hidden');
+}
+
+for(let i = 0 ; i < btnsOpenModal.length; i++){
+  btnsOpenModal[i].addEventListener('click', openModal)
+  
+}
+
+const closeModal = function(){
+  modal.classList.add('hidden')
+  overLay.classList.add('hidden')
+}
+
+btnCloseModal.addEventListener('click', closeModal)
+overLay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function(e){
+  if(e.key == 'Escape'){
+    if(!modal.classList.contains('hidden')){
+    closeModal();
+    }
+  }
+})
+ 
